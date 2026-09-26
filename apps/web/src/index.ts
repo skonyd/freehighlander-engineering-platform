2159ac8ca5d4e90e82680759bcc1bec58481b005
Özgür DAĞLI <skonyd@gmail.com>
2026-09-26 18:07:02 +0300

    feat(web): export Workflow Studio replay preview

--- apps/web/src/index.ts
+++ apps/web/src/index.ts
@@ -1,4 +1,14 @@
 export {
+  buildFhKuikaWorkflowReplayPreviewV1,
+  workflowReplayPreviewCanExecute,
+  workflowReplayPreviewCanGrantAuthority,
+  workflowReplayPreviewCanInvokeModel,
+  type FhKuikaWorkflowReplayPreviewV1,
+  type FhKuikaWorkflowReplayReadSource,
+  type FhKuikaWorkflowReplayStepV1,
+} from './kuika-workflow-replay.js';
+
+export {
   buildFhKuikaWorkflowVersionDiffV1,
   workflowVersionDiffCanGrantAuthority,
   workflowVersionDiffCanPublish,
